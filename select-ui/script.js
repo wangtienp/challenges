5a2d212fcb8ef8861c5189d79dde70348db607c4
wangtienp <wangtienp@gmail.com>
2024-01-26 18:14:45 +0800

    random choice commit

---
+++ select-ui/script.js
@@ -0,0 +1,41 @@
+const textarea = document.querySelector('#input')
+const choices = document.querySelector('.choices')
+
+textarea.addEventListener('input', addChoice)
+
+function addChoice(e) {
+    const value = e.target.value
+    const res = value.split(',')
+    choices.innerHTML = ''
+
+    res.forEach(choice => {
+        if (choice === '') return
+        choices.innerHTML += `<span class='choice'>${choice}</span>`
+    })
+    pickRandomChoice(e)
+}
+function pickRandomChoice(e) {
+    if (e.inputType === 'insertLineBreak') {
+        e.target.value = ''
+        const getChoices = document.querySelectorAll('.choice')
+        //animation for each random choice
+        chooseActive(getChoices)
+    }
+}
+
+function chooseActive(choosen){
+   
+    const id = setInterval(()=>{
+        removeChoosen(choosen)
+        let random = Math.floor(Math.random()*choosen.length)
+        choosen[random].classList.add('choosen')
+    },100)
+    setTimeout(()=>{
+        clearInterval(id)
+    },3000)
+}
+function removeChoosen(choosen){
+    choosen.forEach(choice=>{
+        choice.classList.remove('choosen')
+    })
+}
